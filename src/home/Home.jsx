5c2f8f561798bd9e48e9520e2d47b3beb2e3b151
esteban93040 <espinosa.esteban@correounivalle.edu.co>
2024-12-01 22:09:27 -0500

    prueba sonarcloud

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
-import Graficas from '../genrarGraficas/Graficas';
-import TablaUsuarios from '../TablaUsuarios/TablaUsuarios';
+
 
 const Home = () => {
     return (
