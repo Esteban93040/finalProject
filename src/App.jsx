460b0ae07fec16643b841e5c663291c927d9b24d
esteban93040 <espinosa.esteban@correounivalle.edu.co>
2024-12-01 22:05:02 -0500

    añadiendo yml

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -13,14 +11,6 @@ function App() {
       </main>
     </div>
   );
-}
-
-
-function App() {
-
-  return (
-    <Login />
-  )
 }
 
 export default App;
